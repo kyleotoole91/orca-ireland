edacdddb8548683cdb36d4977c38694f9e0a47b4
kyleotoole91 <49255786+kyleotoole91@users.noreply.github.com>
2021-10-31 03:44:25 +0000

    Endpoints can now be easily added
    Later we can build these based on a collection list query from the database

--- api-express/server.js
+++ api-express/server.js
@@ -29,12 +29,26 @@ app.get('/cors', (req, res) => {
 })
 
 const eventsController = new BaseController('events')
-
-app.get('/events', (req, res) => eventsController.getAllDocuments(req, res))
-app.get('/events/:id', (req, res) => eventsController.getDocument(req, res))
+const usersController = new BaseController('users')
+const membershipsController = new BaseController('memberships')
+//events
+app.get('/events', validateJwt, (req, res) => eventsController.getAllDocuments(req, res))
+app.get('/events/:id', validateJwt, (req, res) => eventsController.getDocument(req, res))
 app.post('/events', validateJwt, (req, res) => eventsController.addDocument(req, res))
 app.put('/events/:id', validateJwt, (req, res) => eventsController.updateDocument(req, res))
 app.delete('/events/:id', validateJwt, (req, res) => eventsController.deleteDocument(req, res))
+//users
+app.get('/users', validateJwt, (req, res) => usersController.getAllDocuments(req, res))
+app.get('/users/:id', validateJwt, (req, res) => usersController.getDocument(req, res))
+app.post('/users', validateJwt, (req, res) => usersController.addDocument(req, res))
+app.put('/users/:id', validateJwt, (req, res) => usersController.updateDocument(req, res))
+app.delete('/users/:id', validateJwt, (req, res) => usersController.deleteDocument(req, res))
+//memberships
+app.get('/memberships', validateJwt, (req, res) => membershipsController.getAllDocuments(req, res))
+app.get('/memberships/:id', validateJwt, (req, res) => membershipsController.getDocument(req, res))
+app.post('/memberships', validateJwt, (req, res) => membershipsController.addDocument(req, res))
+app.put('/memberships/:id', validateJwt, (req, res) => membershipsController.updateDocument(req, res))
+app.delete('/memberships/:id', validateJwt, (req, res) => membershipsController.deleteDocument(req, res))
 
 app.listen(8000, () => {
   console.log('ORCA api server listening on port 8000')
