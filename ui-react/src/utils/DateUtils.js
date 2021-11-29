974ad8e22a7fca82d63f6784adcc8929757e1385
kyleotoole91 <49255786+kyleotoole91@users.noreply.github.com>
2021-11-29 20:50:04 +0000

    Fixed event date issue, rolled back changes

--- ui-react/src/utils/DateUtils.js
+++ ui-react/src/utils/DateUtils.js
@@ -1,12 +1,16 @@
 
-import dayjs from 'dayjs'
-
 export class DateUtils {
   formatDate(date, format) {
     if (!format) {
       format = 'dd/mm/yyyy'
     }
-    return dayjs(date).format(format)
+    const map = {
+      mm: (date.getMonth()+1).toString().padStart(2, '0'),
+      dd: date.getDate().toString().padStart(2, '0'),
+      yy: date.getFullYear().toString().slice(-2),
+      yyyy: date.getFullYear()
+    }
+    return format.replace(/mm|dd|yyyy/gi, matched => map[matched])
   }
   formatISODate(isoDate, format) {
     let date = new Date(isoDate)
