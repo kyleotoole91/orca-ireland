f633e638aac11ceb97fbc1c4c0f8df8087541a36
kyleotoole91 <49255786+kyleotoole91@users.noreply.github.com>
2021-12-04 18:19:26 +0000

    Added printer button
    Which is just an image link to the resource, showing the browser's built in pdf viewer instead of the react component

--- ui-react/src/views/Rules.js
+++ ui-react/src/views/Rules.js
@@ -1,19 +1,19 @@
 import { React } from 'react'
 import { Viewer, Worker } from '@react-pdf-viewer/core'
-//import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'
 import '@react-pdf-viewer/core/lib/styles/index.css'
 import '@react-pdf-viewer/default-layout/lib/styles/index.css'
 import Accordion  from 'react-bootstrap/Accordion'
 import styled from 'styled-components'
 
 function Rules() {
-  //const defaultLayoutPluginInstance = defaultLayoutPlugin()
-  //plugins={[defaultLayoutPluginInstance]} /
   function addAccordianItem(header, eventKey, pdfUrl) {
     return  (
       <Accordion.Item eventKey={eventKey}>
         <StyledAccordionHeader>{header}</StyledAccordionHeader>
-        <Accordion.Body>
+        <Accordion.Body >
+          <a href={pdfUrl}>
+            <img alt="Qries" src='/images/printer.png' style={{width:'24px', height:'24px'}}/>
+          </a>
           <Viewer fileUrl={pdfUrl} />
         </Accordion.Body>
       </Accordion.Item>
