d2544423b5f1f867bf0771e4e5ba4d9825c10a1e
kyleotoole91 <49255786+kyleotoole91@users.noreply.github.com>
2021-12-06 01:32:53 +0000

    Update Home.js
    Improved CSS

--- ui-react/src/views/Home.js
+++ ui-react/src/views/Home.js
@@ -26,9 +26,9 @@ function Home() {
 
   function footer(){
     return(
-      <div>
+      <div style={{maxWidth: '2048px'}}>
         <GoogleMap/>
-        <WhiteText style={{ alignSelf: 'center', textAlign: 'center', display: 'grid',  justifyContent:'center', alignItems:'center', height: 'auto'}}> 
+        <WhiteText style={{display: 'grid'}}> 
           <div style={{marginTop: '6px'}}>
             <a href='https://www.facebook.com/orcaireland/'><img style={{width: '48px', weight: '48px'}} src='/images/facebook.png' alt="Facebook Page"></img></a>  
             <a href={emailTo}>&nbsp;&nbsp;&nbsp;&nbsp;<img style={{width: '36px', weight: '36px'}}  src='/images/email.png' alt="Email"></img></a>
@@ -45,17 +45,15 @@ function Home() {
     return ( <Loading /> )
   } else {
     return (
-      <div style={{width:'100%', height:'100%'}}> 
-        <div style={{ alignSelf: 'center', textAlign: 'center', display: 'grid',  justifyContent:'center', alignItems:'center', height: 'auto'}}>
-          <h1>On Road Circuit Association</h1>
-          <div style={{width: '100%', height: '240px'}}>
-            <img style={{maxWidth: '100%', maxHeight: '100%'}} src={orcaLogo} alt="Circuit Logo"></img>
-          </div>  
-          <h2> Ireland's only 1/8 scale on road circuit</h2>
-          <div style={{width: '100%', height: '100%'}}>
-            <img style={{maxWidth: '100%', maxHeight: '100%'}} src={trackImage} alt="Saint Annes Track"></img>
-          </div> 
-        </div>
+      <div style={{ textAlign: 'center', display: 'grid', justifyContent:'center'}}> 
+        <h1>On Road Circuit Association</h1>
+        <div style={{width: '100%', height: '240px'}}>
+          <img style={{maxWidth: '100%', maxHeight: '100%'}} src={orcaLogo} alt="Circuit Logo"></img>
+        </div>  
+        <h2> Ireland's only 1/8 scale on road circuit</h2>
+        <div style={{width: '100%', height: '100%'}}>
+          <img style={{maxWidth: '100%', maxHeight: '100%'}} src={trackImage} alt="Saint Annes Track"></img>
+        </div> 
         {footer()}
       </div>)
   }
