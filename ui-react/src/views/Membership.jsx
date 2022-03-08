7639ddfc8d5c6ddd5de38dbd206d9a5603b1f336
kyleotoole91 <49255786+kyleotoole91@users.noreply.github.com>
2022-03-08 22:12:17 +0000

    Include emergency contact details in Active Members list

--- ui-react/src/views/Membership.jsx
+++ ui-react/src/views/Membership.jsx
@@ -513,8 +513,12 @@ function Membership() {
         return <Card key={user.extId+'-card'+index} style={{minWidth: '300px', maxWidth: '300px', margin: '3px', zIndex: 0}}>
                  <Card.Header key={user.extId+'-header'+index}>{user.firstName+' '+user.lastName}</Card.Header>
                  <Card.Body key={user.extId+'-body'+index}>
+                   {user.hasOwnProperty('memberType') && <Card.Text key={user.extId+'-type'+index}>Member Type: {user.memberType.name}</Card.Text>}
                    <Card.Text key={user.extId+'-email'+index}>Email: {user.email}</Card.Text>
                    <Card.Text key={user.extId+'-phone'+index}>Phone: {user.phone}</Card.Text>
+                   {user.hasOwnProperty('dateOfBirth') && <Card.Text key={user.extId+'-dob'+index}>DOB: {dateUtils.formatDate(new Date(user.dateOfBirth), 'dd/mm/yyyy')}</Card.Text>}
+                   {user.hasOwnProperty('ecName') && <Card.Text key={user.extId+'-ecName'+index}>Emergency Name: {user.ecName}</Card.Text>}
+                   {user.hasOwnProperty('ecPhone') && <Card.Text key={user.extId+'-ecPhone'+index}>Emergency Phone: {user.ecPhone}</Card.Text>}
                  </Card.Body>
                </Card> 
       } 
